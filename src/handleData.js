8f6ff8355cf511d52fd3b3877a9accc0d084f658
Jasmine Wang <jasminewang76@gmail.com>
2017-09-04 12:20:34 -0600

    handle data

--- src/handleData.js
+++ src/handleData.js
@@ -1,2 +1,10 @@
-const data = require("../data/data.json");
-console.log(data);
+const json = require("../data/data.json");
+const data = json.data;
+
+const countries = data.map(country => country.attributes.name);
+// console.log(countries);
+
+const speedObject = data.map(country => country.relationships.data_points.data[11]);
+
+const speed = speedObject.map(speedObject => speedObject.attributes.value);
+console.log(speed);
